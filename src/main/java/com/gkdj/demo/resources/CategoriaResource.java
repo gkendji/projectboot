d3e2ba563c454bef0680bd6c45b72786cc8ba812
gkendji <guilhermekendji@hotmail.com>
2021-09-05 20:27:25 -0300

    REST testado

---
+++ src/main/java/com/gkdj/demo/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.gkdj.demo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias") // sera nosso ending point
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST esta funcionando";
+	}
+	
+}
