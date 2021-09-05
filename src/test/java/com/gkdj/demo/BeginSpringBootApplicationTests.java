cfdcd7f4dc7724d5362d90a731d2b0a34bf0d174
gkendji <guilhermekendji@hotmail.com>
2021-09-05 19:47:41 -0300

    Projeto criado

---
+++ src/test/java/com/gkdj/demo/BeginSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkdj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
